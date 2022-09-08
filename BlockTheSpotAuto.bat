1218bff49788cb5a2cfe3cce008e30237a95fa84
afdunmore <94896609+afdunmore@users.noreply.github.com>
2022-09-08 16:43:12 +0100

    Update BlockTheSpotAuto.bat

--- BlockTheSpotAuto.bat
+++ BlockTheSpotAuto.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/GrampaSheev/BlockTheSpotAuto/master/install.ps1' | Invoke-Expression}"
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/afdunmore/BlockTheSpotAuto/master/install.ps1' | Invoke-Expression}"
 exit
