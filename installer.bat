b83ffa7e2d51ce7d902a6a274e9f64b4f5cc6abe
afdunmore <94896609+afdunmore@users.noreply.github.com>
2022-12-15 17:32:44 +0000

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,12 +1,13 @@
+@echo off
 cd /D %HOMEDRIVE%\
 cd Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
-del BlockTheSpot.bat
+del BlockTheSpotAuto.bat
 (
-@echo off
+echo @echo off
 echo %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; """"& { $((Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/afdunmore/BlockTheSpotAuto/master/install.ps1').Content)} -confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_on -cache_off -block_update_off -start_spoti """" | Invoke-Expression"
 
-pause
-exit /b
+echo pause
+echo exit /b
 )>"BlockTheSpotAuto.bat"
 
 BlockTheSpotAuto.bat
